4904f6c97dbcf05013f959c536eaac098996b36c
Jz-930 <jiackey.zhang@gmail.com>
2026-02-14 02:37:15 -0500

    fix: suppress ts error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // @ts-expect-error - eslint is a valid config option but missing in local type definition
   eslint: {
     ignoreDuringBuilds: true,
   },
